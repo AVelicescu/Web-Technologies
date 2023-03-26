dfcd4938d84f3eca05d6479fbac1dcc02cd170ff
Ioana Roibu <ioana_roibu@yahoo.com>
2023-03-26 19:14:32 +0300

    cu putin ajutor de la Andrei

--- script.js
+++ script.js
@@ -5,9 +5,6 @@ let create = document.getElementById("containerCreate");
 let forgot = document.getElementById("containerForgot");
 let containers = [home, about, login, create, forgot];
 
-
-let menuNav = document.getElementById("Menu_Nav");
-let settingsNav = document.getElementById("Settings_Nav");
 let menuContainer = document.getElementById("Menu_Container");
 let settingsContainer = document.getElementById("Settings_Container");
 
@@ -20,40 +17,11 @@ function openContainer(container)
     containers[container].style.display = "block";
 }
 
+
 function menu()
 {
-    if(settingsNav.style.visibility === "visible")
-        settings();
-    if(menuNav.style.visibility === "hidden"){
-        menuNav.style.animationName = "OpenMenu";
-        menuNav.style.visibility = "visible";
-    }
-    else if(menuNav.style.visibility === "visible"){
-        menuNav.style.animationName = "CloseMenu";
-        menuNav.style.visibility = "hidden";
-    }
-
-}
-
-function settings()
-{
-    if(menuNav.style.visibility === "visible")
-        menu();
-    if(settingsNav.style.visibility === "hidden"){
-        settingsNav.style.animationName = "OpenMenu";
-        settingsNav.style.visibility = "visible";
-    }
-    else if(settingsNav.style.visibility === "visible"){
-        settingsNav.style.animationName = "CloseMenu";
-        settingsNav.style.visibility = "hidden";
-    }
-
-}
-
-function menuMobile()
-{
     if(settingsContainer.style.visibility === "visible")
-        settingsMobile();
+        settings();
     if(menuContainer.style.visibility === "hidden"){
         menuContainer.style.animationName = "OpenMenu";
         menuContainer.style.visibility = "visible";
@@ -65,9 +33,9 @@ function menuMobile()
 
 }
 
-function settingsMobile() {
+function settings() {
     if (menuContainer.style.visibility === "visible")
-        menuMobile();
+        menu();
     if (settingsContainer.style.visibility === "hidden") {
         settingsContainer.style.animationName = "OpenMenu";
         settingsContainer.style.visibility = "visible";
